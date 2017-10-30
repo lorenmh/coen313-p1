9283802588c121eb076984710223905a3df44038
Manny1995 <immanuelda@gmail.com>
2017-10-30 13:00:43 -0700

    created problem 2 folder, working on a version of it, please either edit mine, or add your own versions :)

---
+++ problem_2/p2_iamirtharaj.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define SIZE 1024
+
+int matrix1[SIZE][SIZE];
+int matrix2[SIZE][SIZE];
+
+void init_matrix() {
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			
+		}
+	}
+
+}
+
+void multiply_regular() {
+	
+	int i;
+	int k;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {       
+			sum = 0;
+ 			for (k = 0; k < n; k++) {
+				sum = sum + a[i][k]*b[k][j];        
+			}       
+			c[i][j] = sum;      
+		}    
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+		
+
+
+	return 1;
+}

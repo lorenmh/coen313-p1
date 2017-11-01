cd36812cc058b81486f14c5853e31b3ad76b5eac
lorenmh <neutronstar@gmail.com>
2017-10-31 23:23:44 -0700

    spaces -> tabs

--- problem_1/part3.c
+++ problem_1/part3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 // 1 meg
 #define BUF_SIZE 1024 * 1024
@@ -12,21 +13,28 @@ uint8_t cbuf[BUF_SIZE];
 uint8_t tbuf[BUF_SIZE];
 
 void clear() {
-    uint8_t v;
-    for (int i = 0; i < BUF_SIZE; i++) v = cbuf[i];
+	uint8_t v;
+	for (int i = 0; i < BUF_SIZE; i++) v = cbuf[i];
 }
 
-int test_assoc(int sz_kb, int assoc) {
-    clear();
+uint32_t utime() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return 1000000 * tv.tv_sec + tv.tv_usec;
+}
 
-    return 0;
+uint32_t time_assoc(int cache_sz_kb, int assoc) {
+	clear();
+	return 0;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("first arg should be cache size in KB\n");
-        exit(1);
-    }
-    uint8_t cache_sz_kb = atoi(argv[1]);
-    printf("Using cache size of %d KB\n", cache_sz_kb);
+	if (argc != 2) {
+		printf("first arg should be cache size in KB\n");
+		exit(1);
+	}
+
+	int cache_sz_kb = atoi(argv[1]);
+
+	printf("Using cache size of %d KB\n", cache_sz_kb);
 }

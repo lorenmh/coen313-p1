200eb57f1dfff09b6de24b0612f9d25b77607645
Manny1995 <immanuelda@gmail.com>
2017-11-01 18:55:47 -0700

    got tiling to work

--- problem_1/part2.cpp
+++ problem_1/part2.cpp
@@ -1,5 +1,7 @@
 // Immanuel Amirtharaj
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <time.h>
 #include <sys/time.h>
@@ -10,109 +12,52 @@
 
 using namespace std;
 
-#define SIZE 1000000000L
+#define SIZE 10000
 
-char large_array[SIZE];
-
-static struct timeval start_time;
-static struct timeval end_time;
-
-
-// https://www.gnu.org/software/libc/manual/html_node/Elapsed-Time.html
-int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
-{
-  /* Perform the carry for the later subtraction by updating y. */
-  if (x->tv_usec < y->tv_usec) {
-    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-    y->tv_usec -= 1000000 * nsec;
-    y->tv_sec += nsec;
-  }
-  if (x->tv_usec - y->tv_usec > 1000000) {
-    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-    y->tv_usec += 1000000 * nsec;
-    y->tv_sec -= nsec;
-  }
-
-  /* Compute the time remaining to wait.
-     tv_usec is certainly positive. */
-  result->tv_sec = x->tv_sec - y->tv_sec;
-  result->tv_usec = x->tv_usec - y->tv_usec;
-
-  /* Return 1 if result is negative. */
-  return x->tv_sec < y->tv_sec;
-}
-
-
-double get_diff(struct timeval elapsed) {
-	cout << elapsed.tv_sec << endl;
-	cout << elapsed.tv_usec << endl;
-	float elapsed_ms = ((elapsed.tv_sec * 1000.0) + (elapsed.tv_usec / 1000.0));
-	// float elapsed_seconds = elapsed_ms / 1000.0;
-	return elapsed_ms;
-
-}
-
+uint8_t *large_array;
 
 void init_array() {
-	srand(time(NULL));
+	// srand(time(NULL));
+
+	// for (int i = 0; i < SIZE; i++) {
+	// 	large_array[i] = (char) (rand() % 256);
+	// }
+
+	uint8_t *temp_array = (uint8_t *) malloc(SIZE * sizeof(uint8_t));
+
+	uint8_t offset = (uint64_t) temp_array % (32 * 1024);
+
+	large_array = &temp_array[(32 * 1024) - offset]; // alligned to cache size
+
 
 	for (int i = 0; i < SIZE; i++) {
-		large_array[i] = (char) (rand() % 256);
+		large_array[i] = temp_array[i * (32 * 1024)];
 	}
+
 	cout << "Finished initializing array" << endl;
 }
 
-// Takes in an integer of size in BYTES
+// Takes in an integer of size in bytes
 void run_experiment(int guess_size) {
 
 	// because indexed from 0 to n-1
 	int first_pos = guess_size - 1;
 	int second_pos = guess_size;
 
-	clock_t start, end;
-
-
-	start = clock();
-	//gettimeofday(&start_time, NULL);
+	struct timeval start_time, end_time;
 
 	// Brings the cache line into memory
-	int first = large_array[first_pos];
-	end = clock();
+	int first_element = large_array[first_pos];
+
+	// Calculate the time it takes to access the next element
+	gettimeofday(&start_time, NULL);
+	int next_element = large_array[second_pos];
 	gettimeofday(&end_time, NULL);
 
-	//struct timeval elapsed;
-	//timeval_subtract(&elapsed, &end_time, &start_time);
-
-	//double time_for_first = get_diff(elapsed);
-	double time_for_first = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-
-	//gettimeofday(&start_time, NULL);
-	start = clock();
-	// Access last element
-	int last = large_array[second_pos];
-	
-	//gettimeofday(&end_time, NULL);
-	end = clock();
-
-	//timeval_subtract(&elapsed, &end_time, &start_time);
-
-	//double time_for_last = get_diff(elapsed);
-
-	double time_for_last = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-
-	cout << "\n\n" << endl;
-	cout << "RESULTS FOR GUESS SIZE OF " << guess_size << endl;
-	cout << "The time for " << first_pos << " is " << time_for_first << endl;
-	cout << "The time for " << second_pos << " is " << time_for_last << endl;
-
-	if (time_for_last < time_for_first) {
-		//cout << "This is probably on the same cache line" << endl;
-	}
-	else {
-		cout << "This is probably on a different cache line" << endl;
-	}
+	double time_for_last = (end_time.tv_usec) - (start_time.tv_usec);
+	cout << start_time.tv_sec << " and " << start_time.tv_usec << endl;
+	cout << end_time.tv_sec << " and " << start_time.tv_usec << endl;
+	cout << "time for last is 	" << time_for_last << endl;
 
 }
 
@@ -122,8 +67,7 @@ int main(int argc, char* argv[]) {
 
 	for (int i = 4; i < 11; i++) {
 		int guess_size = pow(2, i);
-		run_experiment(guess_size);
-		//init_array();
+	//	run_experiment(guess_size);
 	}
 
 
--- problem_2/p2_iamirtharaj.c
+++ problem_2/p2_iamirtharaj.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#define SIZE 4
 
-#define SIZE 1024
+int a[SIZE][SIZE];
+int b[SIZE][SIZE];
 
-int matrix1[SIZE][SIZE];
-int matrix2[SIZE][SIZE];
-
-int matrix3[SIZE][SIZE];
+int c[SIZE][SIZE];
 
 void init_matrix() {
 	int i = 0;
@@ -15,26 +15,55 @@ void init_matrix() {
 
 	for (i = 0; i < SIZE; i++) {
 		for (j = 0; j < SIZE; j++) {
-			matrix1[i][j] = rand() % 10;
-			matrix2[i][j] = rand() % 10;			
+			a[i][j] = rand() % 10;
+			b[i][j] = rand() % 10;			
 		}
 	}
 
 }
 
+
+int min(int a, int b) {
+	if (b < a) {
+		return b;
+	}
+
+	return a;
+}
+
+void multiply_tiling(int block) {
+
+	int n = SIZE;
+	int p = SIZE;
+	int m = SIZE;
+
+	for (int i = 0; i < n; i+=block) {
+		for (int j = 0; j < p; j+=block) {
+			for (int k = 0; k < m; k+=block) {
+				for (int ii = i; ii < min(i + block, n); ii++) {
+					for (int jj = j; jj < min(j+block, p); jj++) {
+						int sum = 0;
+						for (int kk = k; kk < min(k+block, m); kk++) {
+							int temp = a[ii][kk] * b[kk][jj];
+							sum+=temp;
+						}
+						c[ii][jj]+=sum;
+					}
+				}
+			}
+		}
+	}
+}
+
 void multiply_regular() {
 	
-	int i;
-	int j;
-	int k;
-
-	for (i = 0; i < SIZE; i++) {
-		for (j = 0; j < SIZE; j++) {       
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {       
 			int sum = 0;
- 			for (k = 0; k < SIZE; k++) {
-				sum = sum + matrix1[i][k]*matrix2[k][j];        
+ 			for (int k = 0; k < SIZE; k++) {
+				sum = sum + a[i][k]*b[k][j];        
 			}       
-			matrix3[i][j] = sum;      
+			c[i][j] = sum;      
 		}    
 	}
 }
@@ -59,14 +88,18 @@ int main(int argc, char *argv[]) {
 	struct timespec start_time;
 	struct timespec end_time;
 	
-	gettimeofday(&start_time, NULL);
+//	gettimeofday(&start_time, NULL);
+//	multiply_regular();
+	multiply_tiling(2);
+//	gettimeofday(&end_time, NULL);
+	print_matrix(a);
+	printf("\n");
+	print_matrix(b);
+	printf("\n");
+	print_matrix(c);
+
 	multiply_regular();
-	gettimeofday(&end_time, NULL);
-
-	printf("%ld\n", ((end_time.tv_sec * 1000000000 + end_time.tv_nsec) - (start_time.tv_sec * 1000000000 + start_time.tv_nsec)));
-	
-		
-//	print_matrix(matrix3);
-
+	printf("\n");
+	print_matrix(c);
 	return 1;
 }
